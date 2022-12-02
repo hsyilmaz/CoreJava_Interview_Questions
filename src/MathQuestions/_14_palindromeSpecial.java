7028708f3b6d8b27d44dd090e0259f6eba2c0846
Huseyin YILMAZ <hsyilmaz1979@gmail.com>
2022-12-02 18:13:45 +0300

    .

--- src/MathQuestions/_14_palindromeSpecial.java
+++ src/MathQuestions/_14_palindromeSpecial.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 
 public class _14_palindromeSpecial {
     public static void main(String[] args) {
-        Scanner oku= new Scanner(System.in);
-        System.out.print("Bir sayi giriniz: ");
-        int sayı= oku.nextInt();
+        Scanner scan= new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number= scan.nextInt();
 
-        int sayac=0;
-        while(sayı!=tersBulma(sayı))
-        { sayı = sayı+tersBulma(sayı); sayac++; }
+        int counter=0;
+        while(number!=reverseFinder(number))
+        { number = number+reverseFinder(number); counter++; }
 
-        System.out.println("sayac = " + sayac);
-        System.out.println("sayı = " + sayı);
+        System.out.println("counter = " + counter);
+        System.out.println("number = " + number);
     }
-    public static int tersBulma(int num){
-        int ters=0;
+    public static int reverseFinder(int num){
+        int reverse=0;
         while(num>0)
-        { ters=ters*10+num%10; num/=10;}
-        return ters;
+        { reverse=reverse*10+num%10; num/=10;}
+        return reverse;
     }
 }
